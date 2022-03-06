564bbf6f799829937d5c3de4e2806b0f3d55f8d4
Bujare <bujare@frakton.com>
2022-03-06 03:08:49 +0100

    single blog page changes

--- single-blog.php
+++ single-blog.php
@@ -2,6 +2,45 @@
 
 require "admin/includes/dbh.php";
 
+if (isset($_REQUEST['blog'])) {
+
+    $blogPath = $_REQUEST['blog'];
+
+    $sqlGetBlog = "SELECT * FROM blog_post WHERE post_path = '$blogPath' AND post_status = '1'";
+    $queryGetBlog = mysqli_query($connection, $sqlGetBlog);
+
+    if ($rowGetBlog = mysqli_fetch_assoc($queryGetBlog)) {
+        $blogPostId = $rowGetBlog['blog_post_id'];
+        $blogCategoryId = $rowGetBlog['category_id'];
+        $blogTitle = $rowGetBlog['post_title'];
+        $blogMetaTitle = $rowGetBlog['post_meta_content'];
+        $blogContent = $rowGetBlog['post_title'];
+        $blogMainImgUrl = $rowGetBlog['main_image_url'];
+        $blogCreationDate = $rowGetBlog['date_created'];
+    }
+    else {
+        header("Location: index.php");
+        exit();
+    }
+
+    $sqlGetCategory = "SELECT * FROM blog_category WHERE category_id = '$blogCategoryId'";
+    $queryGetCategory = mysqli_query($connection, $sqlGetCategory);
+
+    if ($rowGetCategory = mysqli_fetch_assoc($queryGetCategory)) {
+        $categoryTitle = $rowGetCategory['category_title'];
+        $blogCategoryPath = $rowGetCategory['category_path'];
+    }
+
+    $sqlGetTags = "SELECT * FROM blog_tags WHERE blog_post_id = '$blogPostId'";
+    $queryGetTags = mysqli_query($connection, $sqlGetTags);
+
+    if ($rowGetTags = mysqli_fetch_assoc($queryGetTags)) {
+        $blogTags = $rowGetTags['tag'];
+        $blogTagArr = explode(",", $blogTags);
+    }
+
+}
+
 ?>
 
 <!DOCTYPE html>
@@ -11,7 +50,7 @@ require "admin/includes/dbh.php";
     <!--- basic page needs
     ================================================== -->
     <meta charset="utf-8">
-    <title>Standard Post - Calvin</title>
+    <title>Bujare's Blog | <?php echo $blogMetaTitle; ?></title>
     <meta name="description" content="">
     <meta name="author" content="">
 
@@ -63,129 +102,22 @@ require "admin/includes/dbh.php";
 
                     <div class="s-content__media">
                         <div class="s-content__post-thumb">
-                            <img src="images/thumbs/single/single-post-1050.jpg" 
-                                 srcset="images/thumbs/single/single-post-2100.jpg 2100w, 
-                                         images/thumbs/single/single-post-1050.jpg 1050w, 
-                                         images/thumbs/single/single-post-525.jpg 525w" sizes="(max-width: 2100px) 100vw, 2100px" alt="">
+                            <img src="<?php echo $blogMainImgUrl; ?>" 
+                                 srcset="<?php echo $blogMainImgUrl; ?> 2100w, 
+                                 <?php echo $blogMainImgUrl; ?> 1050w, 
+                                 <?php echo $blogMainImgUrl; ?> 525w" sizes="(max-width: 2100px) 100vw, 2100px" alt="">
                         </div>
                     </div> <!-- end s-content__media -->
 
                     <div class="s-content__entry-header">
-                        <h1 class="s-content__title s-content__title--post">Hey, This Is A Standard Format Post.</h1>
+                        <h1 class="s-content__title s-content__title--post"><?php echo $blogTitle ?></h1>
                     </div> <!-- end s-content__entry-header -->
 
                     <div class="s-content__primary">
 
                         <div class="s-content__entry-content">
 
-                            <p class="lead">
-                            Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor 
-                            sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat 
-                            enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco 
-                            magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum 
-                            adipisicing aliqua ea nisi sint. Unde quod at minus quia velit ipsa ea qui. </p> 
-    
-                            <p>
-                            Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor 
-                            sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat 
-                            enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco 
-                            magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum 
-                            adipisicing aliqua ea nisi sint ut quis proident ullamco ut dolore culpa occaecat ut 
-                            laboris in sit minim cupidatat ut dolor voluptate enim veniam consequat occaecat fugiat 
-                            in adipisicing in amet Ut nulla nisi non ut enim aliqua laborum mollit quis nostrud sed sed.
-                            </p>
-    
-                            <p>
-                                <img src="images/sample-1050.jpg" 
-                                        srcset="images/sample-2100.jpg 2100w, 
-                                                images/sample-1050.jpg 1050w, 
-                                                images/sample-525.jpg 525w" sizes="(max-width: 2100px) 100vw, 2100px" alt="">
-                            </p>
-    
-                            <p>
-                            Duis ex ad cupidatat tempor Excepteur cillum cupidatat fugiat nostrud cupidatat dolor 
-                            sunt sint sit nisi est eu exercitation incididunt adipisicing veniam velit id fugiat 
-                            enim mollit amet anim veniam dolor dolor irure velit commodo cillum sit nulla ullamco 
-                            magna amet magna cupidatat qui labore cillum sit in tempor veniam consequat non laborum 
-                            adipisicing aliqua ea nisi sint ut quis proident ullamco ut dolore culpa occaecat ut 
-                            laboris in sit minim cupidatat ut dolor voluptate enim veniam consequat occaecat fugiat 
-                            in adipisicing in amet Ut nulla nisi non ut enim aliqua laborum mollit quis nostrud sed sed.
-                            </p>
-    
-                            <h2>Large Heading</h2>
-    
-                            <p>
-                            Harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta 
-                            nobis est eligendi optio cumque nihil impedit quo minus <a href="http://#">omnis voluptas assumenda est</a> 
-                            id quod maxime placeat facere possimus, omnis dolor repellendus. Temporibus autem quibusdam et 
-                            aut officiis debitis aut rerum necessitatibus saepe eveniet ut et.</p>
-    
-                            <blockquote>
-                                <p>
-                                For God so loved the world, that he gave his only Son, that whoever believes in 
-                                him should not perish but have eternal life. For God did not send his Son into 
-                                the world to condemn the world, but in order that the world might be 
-                                saved through him.
-                                </p>
-                                <cite>John 3:16-17 ESV</cite>
-                            </blockquote>
-    
-                            <p>
-                            Odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti dolores 
-                            et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa. 
-                            Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit 
-                            libero, a pharetra augue laboris in sit minim cupidatat ut dolor voluptate enim veniam consequat 
-                            occaecat fugiat in adipisicing in amet Ut nulla nisi non ut enim aliqua laborum mollit quis nostrud sed sed..</p>
-    
-                            <h3>Smaller Heading</h3>
-    
-                            <p>
-                            Quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est 
-                            eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas 
-                            assumenda est, omnis dolor repellendus.
-                            </p>
-    
-                            <p>
-                            Quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est 
-                            eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas 
-                            assumenda est, omnis dolor repellendus.
-                            </p>
-                            
-<pre><code class="language-css">
-    code {
-        font-size: 1.4rem;
-        margin: 0 .2rem;
-        padding: .2rem .6rem;
-        white-space: nowrap;
-        background: #F1F1F1;
-        border: 1px solid #E1E1E1;	
-        border-radius: 3px;
-    }
-</code></pre>
-                                
-                            <p>
-                            Odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti dolores et 
-                            quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa.</p>
-    
-                            <ul>
-                                <li>Donec nulla non metus auctor fringilla.
-                                    <ul>
-                                        <li>Lorem ipsum dolor sit amet.</li>
-                                        <li>Lorem ipsum dolor sit amet.</li>
-                                        <li>Lorem ipsum dolor sit amet.</li>
-                                    </ul>
-                                </li>
-                                <li>Donec nulla non metus auctor fringilla.</li>
-                                <li>Donec nulla non metus auctor fringilla.</li>
-                            </ul>
-    
-                            <p>
-                            Odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti dolores et quas 
-                            molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa. Aenean eu leo quam. 
-                            Pellentesque ornare sem lacinia quam venenatis vestibulum. Nulla vitae elit libero, a pharetra augue 
-                            laboris in sit minim cupidatat ut dolor voluptate enim veniam consequat occaecat fugiat in adipisicing 
-                            in amet Ut nulla nisi non ut enim aliqua laborum mollit quis nostrud sed sed.
-                            </p>
+                            <?php echo $blogContent; ?>
 
                         </div> <!-- end s-entry__entry-content -->
 
@@ -197,7 +129,7 @@ require "admin/includes/dbh.php";
                                 </div>
                                 <div class="byline">
                                     <span class="bytext">Posted By</span>
-                                    <a href="#0">John Doe</a>
+                                    <a href="#">Bujare Ndrecaj</a>
                                 </div>
                             </div>
 
@@ -206,9 +138,7 @@ require "admin/includes/dbh.php";
                                 <div class="entry-cat-links meta-blk">
                                     <div class="cat-links">
                                         <span>In</span> 
-                                        <a href="#0">Frontend</a>
-                                        <a href="#0">Design</a>
-                                        <a href="#0">Work</a>
+                                        <a href="categories.php?group=<?php echo $blogCategoryPath; ?>"><?php echo $categoryTitle; ?></a>
                                     </div>
 
                                     <span>On</span>
